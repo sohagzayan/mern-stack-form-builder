d319930fb99e9b511727989776f7913692386aea
sohag.zayan <gsohag360@gmail.com>
2023-02-02 00:34:10 -0800

    add form bulder component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>hello bangladesh</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FormBuilder from "../components/FormBuilder/FormBuilder";
+
+const Home = () => {
+  return (
+    <div>
+      <FormBuilder />
+    </div>
+  );
+};
+
+export default Home;
